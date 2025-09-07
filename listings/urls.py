7743cef0ef63bb47517217d548bf6b748ec235f3
Priscilla Alakara <140266837+P-AlaKara@users.noreply.github.com>
2025-09-07 21:51:40 +0300

    Delete listings directory

--- listings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.http import JsonResponse
-
-def dummy_view(request):
-    return JsonResponse({"message": "Hello from listings API!"})
-
-urlpatterns = [
-    path("", dummy_view, name="listings-home"),
-]
